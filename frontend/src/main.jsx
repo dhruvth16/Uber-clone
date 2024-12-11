5255b3cb59ea3f86a8cf4a6ff8209fae67375ab1
dhruvth16 <dhruvthakur1616@gmil.com>
2024-12-11 14:18:51 +0530

    two-way binding

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,29 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
-import UserLogin from './pages/UserLogin.jsx';
-import CaptainLogin from './pages/CaptainLogin.jsx';
-import UserSignup from './pages/UserSignup.jsx';
-import CaptainSignup from './pages/CaptainSignup.jsx';
+import UserLogin from "./pages/UserLogin.jsx";
+import CaptainLogin from "./pages/CaptainLogin.jsx";
+import UserSignup from "./pages/UserSignup.jsx";
+import CaptainSignup from "./pages/CaptainSignup.jsx";
+import UserContext from "./context/UserContext.jsx";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/userLogin" element={<UserLogin />} />
-      <Route path="/captainLogin" element={<CaptainLogin />} />
-      <Route path="/userSignup" element={<UserSignup />} />
-      <Route path="/captainSignup" element={<CaptainSignup />} />
-    </Routes>
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <UserContext>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/userLogin" element={<UserLogin />} />
+          <Route path="/captainLogin" element={<CaptainLogin />} />
+          <Route path="/userSignup" element={<UserSignup />} />
+          <Route path="/captainSignup" element={<CaptainSignup />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext>
+  </StrictMode>
+);
